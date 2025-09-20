0f7b3d388c17f29695a486d6e7ef25cdc594e6e8
RahulM4 <rahulrps447@gmail.com>
2025-09-20 19:32:02 +0530

    update

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,24 @@
-// api/index.js
-const app = require('../server/index.js'); 
-module.exports = app;                    
+const http = require('http');
+const app = require('../app');
+const config = require('../config/env');
+const { connectDb } = require('../config/database');
+
+const server = http.createServer(app);
+
+const start = async () => {
+  try {
+    await connectDb();
+    server.listen(config.port, () => {
+      console.log(`API running on port ${config.port}`);
+    });
+  } catch (error) {
+    console.error('Failed to start server', error);
+    process.exit(1);
+  }
+};
+
+if (require.main === module) {
+  start();
+}
+
+module.exports = { app, start };
